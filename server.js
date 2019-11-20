9155da17b52ca4fb782f902b260c6cace299fbfd
Upneet <captainus55@gmail.com>
2019-11-19 21:28:38 -0500

    Continue building home page and FAQ page

--- server.js
+++ server.js
@@ -20,14 +20,41 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.render('index', {
         newsPosts: [
-            { heading: "Hello", content: "This is the post content" },
-            { heading: "Heading2", content: "This is the post content 2" }
+            { heading: "Hello", content: "This is the post content This is the post content This is the post content This is the post content This is the post content This is the post content This is the post content This is the post content This is the post content This is the post content This is the post content " },
+            { heading: "Hello", content: "This is the post content 2" },
+            { heading: "Hello", content: "This is the post content 3" }
         ]
     });
 });
 
 app.get('/FAQ', (req, res) => {
-    res.render('FAQ');
+    res.render('FAQ', {
+        questionsList: [{
+                question: "What does CASIIT stand for?",
+                answer: "CASIIT stands for Center for Applied Sciences, Interactive and Information Technology."
+            },
+            {
+                question: "Can I pursue multiple paths at once?",
+                answer: "Yes, in fact, we encourage students to try many of the wide variety of courses that CASIIT has to offer."
+            },
+            {
+                question: "I missed joining CASIIT as a freshman, can I still join the program?",
+                answer: "Students can join CASIIT in any year by applying to the program in January."
+            },
+            {
+                question: "Why the switch from the IT Program?",
+                answer: "The new CASIIT program better encompasses the course options and pathways that we offer, they can not simply be classified as Information Technology. Our new name also allows for growth in our program."
+            },
+            {
+                question: "Why do I need to take ITE115?",
+                answer: "ITE115 is Computer Applications and Concepts. The concepts in this course are required by almost every college in every degree program, and it is the prerequisite to all dual enrolled IT courses at Battlefield High School and NVCC."
+            },
+            {
+                question: "Is there any cost for the courses offered by the CASIIT Program?",
+                answer: "Dual enrollment courses will require the cost of the associated college book/s."
+            }
+        ]
+    });
 });
 
 app.get('/dual-enrollment', (req, res) => {
