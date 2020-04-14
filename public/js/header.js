3b97088eb6d38285c9129dc3a5a85077ed561fe1
H S <captainus55@gmail.com>
2020-04-13 23:37:48 -0400

    Home Page redesign in progress - Completed opening section; Pushing important files

---
+++ public/js/header.js
@@ -0,0 +1,15 @@
+function adjustHeaderColor() {
+    if ($(window).scrollTop() > 150) {
+        document.getElementById("header").style.backgroundColor = "#f5f5f5";
+        document.getElementById("headerLogo").style.display = "inline-block";
+        document.getElementById("headerLogo").style.opacity = "1";
+    }
+    else {
+        document.getElementById("header").style.backgroundColor = "#ffffff";
+        document.getElementById("headerLogo").style.display = "none";
+        document.getElementById("headerLogo").style.opacity = "0";
+
+    }
+}
+
+document.body.onscroll = adjustHeaderColor;
