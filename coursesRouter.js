fc9f4f859e1bb9d77f8139cd3edb98569a0a2fb2
H S <captainus55@gmail.com>
2020-06-27 16:04:01 -0400

    Finished courses pages

--- coursesRouter.js
+++ coursesRouter.js
@@ -52,11 +52,81 @@ router.get('/infoTechCourses', (req, res) => {
 });
 
 router.get('/interactiveMedia', (req, res) => {
-    res.render('interactiveMedia');
+    res.render('interactiveMedia', {
+        interactiveMediaCourses: [
+            {
+                id: "#915060",
+                name: "Advanced Placement Studio Art (Drawing)"
+            },
+            {
+                id: "#914860",
+                name: "Advanced Placement Studio Art (2-D Design)"
+            },
+            {
+                id: "",
+                name: "Pre-AP Art Portfolio"
+            },
+            {
+                id: "#915320",
+                name: "CASIIT Graphic Design"
+            },
+            {
+                id: "#918000",
+                name: "CASIIT Computer Graphics I"
+            },
+            {
+                id: "#918100",
+                name: "CASIIT Computer Graphics II"
+            },
+            {
+                id: "#918001",
+                name: "CASIIT Multimedia Software Design and Development I"
+            },
+            {
+                id: "#919020",
+                name: "Photography I"
+            },
+            {
+                id: "#919120",
+                name: "Photography II"
+            }
+        ]
+    });
 });
 
 router.get('/appliedSciences', (req, res) => {
-    res.render('appliedSciences');
+    res.render('appliedSciences', {
+        appliedSciencesCourses: [
+            {
+                id: "#318400",
+                name: "Advanced Computer Mathematics (Java)"
+            },
+            {
+                id: "#318560",
+                name: "Advanced Placement Computer Science A (Java)"
+            },
+            {
+                id: "",
+                name: "Data Structures And Algorithms"
+            },
+            {
+                id: "",
+                name: "Advanced Computer Studies"
+            },
+            {
+                id: "#845020",
+                name: "Engineering Exploration / Robotics I"
+            },
+            {
+                id: "#845100",
+                name: "Engineering Analysis and Applications / Robotics II"
+            },
+            {
+                id: "#841420",
+                name: "Sustainability and Renewable Technology"
+            }
+        ]
+    });
 });
 
 router.use(express.static('public'));
