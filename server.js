2456af159d6b0049cf1a616e59694bdba5214cf6
RoboCrazy <captainus55@gmail.com>
2020-12-11 18:11:02 -0500

    Made minor design changes and added live-reload for server and browser to improve workflow

--- server.js
+++ server.js
@@ -195,6 +195,6 @@ app.post('/sendEmail', [
 //     }
 // })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
